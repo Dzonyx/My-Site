7b63b3d255044c3ff3e5e809d958f2d0ef1f6055
Nikola Radovic <nocodebuilder@hotmail.com>
2025-11-21 12:59:52 +0100

    Refactor genId and add downloadBlob utility function
    Refactor genId function and add downloadBlob function.

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,2 @@
-// src/utils.js
-function genId(prefix='id') {
-  return prefix + '_' + Math.random().toString(36).slice(2,9);
-}
-
-function escapeHtml(s=''){ return String(s).replace(/[&<>"']/g, function(m){return {'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[m];}); }
+function genId(prefix='id'){ return prefix+'_'+Math.random().toString(36).substr(2,9);}
+function downloadBlob(filename, content, type='text/html'){ const a=document.createElement('a'); const blob=new Blob([content],{type}); a.href=URL.createObjectURL(blob); a.download=filename; a.click(); setTimeout(()=>URL.revokeObjectURL(a.href),1000);}
