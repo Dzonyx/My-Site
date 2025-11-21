330516acd4b6a32eed49625e539f21cbb8fc7963
Nikola Radovic <nocodebuilder@hotmail.com>
2025-11-21 11:38:12 +0100

    Delete supabase directory

--- supabase/migrations/20251020095239_49306da0-37de-457b-81da-0a889f1b2e19.sql
+++
@@ -1,7 +0,0 @@
--- Add owner role to the enum
-DO $$ 
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_enum WHERE enumlabel = 'owner' AND enumtypid = 'public.app_role'::regtype) THEN
-    ALTER TYPE public.app_role ADD VALUE 'owner';
-  END IF;
-END $$;
